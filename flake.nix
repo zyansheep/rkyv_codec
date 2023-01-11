8717e68720dc83c3f78e0047400844c40bd823e7
zyansheep <zyansheep@protonmail.com>
2023-01-10 19:32:06 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,6 @@
 {
-	inputs = {
-		nixCargoIntegration.url = "github:yusdacra/nix-cargo-integration";
-	};
-	outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
-		root = ./.;
-		overrides = {
-			shell = common: prev: with common.pkgs; {
-				# hardeningDisable = [ "fortify" ];
-				env = prev.env ++ [
-					{
-						# Vulkan doesn't work without this for some reason :(
-						name = "LD_LIBRARY_PATH";
-						eval = "$LD_LIBRARY_PATH${pkgs.vulkan-loader}/lib";
-					}
-					{
-						name = "NIX_CFLAGS_LINK";
-						eval = "-fuse-ld=lld";
-					}
-				];
-			};
-		};
-	};
+  inputs = {
+    nci.url = "github:yusdacra/nix-cargo-integration";
+  };
+  outputs = inputs: inputs.nci.lib.makeOutputs { root = ./.; };
 }
