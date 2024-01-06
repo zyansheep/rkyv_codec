2dbd472f051cb6b0445982e128512c1f9589ccb3
zyansheep <zyansheep@protonmail.com>
2024-01-05 19:23:20 -0500

    fix flake (move away from nci, it keeps causing problems) :'(

--- flake.nix
+++ flake.nix
@@ -1,40 +1,36 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
-  inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.parts.url = "github:hercules-ci/flake-parts";
-  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  description = "Rust Overlay";
 
-  outputs = inputs @ {
-    parts,
-    nci,
-    ...
-  }:
-    parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
-      imports = [nci.flakeModule];
-      perSystem = {config, ...}: let
-        # TODO: change this to your crate's name
-        crateName = "rkyv-codec";
-        # shorthand for accessing this crate's outputs
-        # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
-        crateOutputs = config.nci.outputs.${crateName};
-      in {
-        # declare projects
-        # relPath is the relative path of a project to the flake root
-        # TODO: change this to your crate's path
-        nci.projects.${crateName}.relPath = "";
-        # configure crates
-        nci.crates.${crateName} = {
-          # export crate (packages and devshell) in flake outputs
-          # alternatively you can access the outputs and export them yourself (see below)
-          export = true;
-          # look at documentation for more options
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-        # export the crate devshell as the default devshell
-        devShells.default = crateOutputs.devShell;
-        # export the release package of the crate as default package
-        packages.default = crateOutputs.packages.release;
-      };
-    };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+          '';
+        };
+      }
+    );
 }
